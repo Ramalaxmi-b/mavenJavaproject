343b7205316509147e1ee15760db972db4092d88
Ramalaxmi-b <ramalaxmib671@gmail.com>
2024-11-23 15:38:11 +0530

    maven java commit

---
+++ src/main/java/com/exampleproject/SampleJavaMaven/App.java
@@ -0,0 +1,13 @@
+package com.exampleproject.SampleJavaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
